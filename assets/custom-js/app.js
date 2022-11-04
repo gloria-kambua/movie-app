d14bf182aa95d104eac49669bec9e2125085ced0
kambua <glorias@digifintek.com>
2022-11-04 20:02:31 +0300

    created the html and the css files

---
+++ assets/custom-js/app.js
@@ -0,0 +1,3 @@
+const APILINK='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ad03db722cabd3857ccb826571257a6a&page=1';
+const IMG_PATH='https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI='https://api.themoviedb.org/3/search/movie?&api_key=ad03db722cabd3857ccb826571257a6a&query=""';
